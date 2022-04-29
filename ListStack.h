5d3c5fdec3c49a525c070267be81fe236c1f1be2
KseniaImprota <103604689+KseniaImprota@users.noreply.github.com>
2022-04-29 18:57:38 +0300

    Create ListStack.h

---
+++ ListStack.h
@@ -0,0 +1,26 @@
+pragma once
+
+#include "StackImplementation.h"
+#include <list>
+
+using namespace std;
+
+class ListStack : public IStackImplementation {
+private:
+    list<ValueType> _list;
+  
+public:
+    ListStack() = default;
+    
+    ListStack(const ValueType* rawArray, const size_t size);
+
+    void push(const ValueType& value);
+
+    void pop();
+
+    const ValueType& top() const;
+
+    bool isEmpty() const;
+
+    size_t size() const;
+};
