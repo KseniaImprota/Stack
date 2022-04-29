be7189eff213e6046a51aa986b27eb01855c3bfe
KseniaImprota <103604689+KseniaImprota@users.noreply.github.com>
2022-04-29 18:51:33 +0300

    Create VectorStack.cpp

---
+++ VectorStack.cpp
@@ -0,0 +1,28 @@
+#include "VectorStack.h"
+#include <vector>
+
+using namespace std;
+
+VectorStack::VectorStack(const ValueType* rawArray, const size_t size) {
+    _vector.insert(_vector.begin(), rawArray, rawArray + size);
+}
+
+void VectorStack::push(const ValueType& value) {
+    _vector.push_back(value);
+}
+
+void VectorStack::pop() {
+    _vector.pop_back();
+}
+
+const ValueType& VectorStack::top() const {
+    return  _vector.back();
+}
+
+bool VectorStack::isEmpty() const {    
+    return _vector.empty();
+}
+
+size_t VectorStack::size() const {
+    return _vector.size();
+}
