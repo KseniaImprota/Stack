2d74bdfe09d960198f9e386005780757e15172bd
KseniaImprota <103604689+KseniaImprota@users.noreply.github.com>
2022-04-29 19:01:57 +0300

    Create ListStack.cpp

---
+++ ListStack.cpp
@@ -0,0 +1,28 @@
+#include "ListStack.h"
+#include <list>
+
+using namespace std;
+
+ListStack::ListStack(const ValueType* rawArray, const size_t size) {
+    _list.insert(_list.begin(), rawArray, rawArray + size);
+}
+
+void ListStack::push(const ValueType& value) {
+    _list.push_back(value);
+}
+
+void ListStack::pop() {
+    _list.pop_back();
+}
+
+const ValueType& ListStack::top() const {
+    return  _list.back();
+}
+
+bool ListStack::isEmpty() const {    
+    return _list.empty();
+}
+
+size_t ListStack::size() const {
+    return _list.size();
+}
